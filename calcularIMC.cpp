7149d8faa7b01ca30ae730c23b1fd06897173e24
Isa Vasconcelos <166054720+isavasconcelos@users.noreply.github.com>
2026-08-26 00:55:01 +0000

    funcao pronta

--- calcularIMC.cpp
+++ calcularIMC.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 //Seção de importação
-float calcularIMC(char, float, float);
+float calcularIMC(char[], float, float);
 
 int main(){
     char nome[50];
@@ -12,4 +13,14 @@ int main(){
     scanf("%f", &peso);
     printf("Escreva sua altura: ");
     scanf("%f", &altura);
+
+    calcularIMC(nome, peso, altura);
 } 
+
+float calcularIMC(char nome[], float peso, float altura){
+float imc;
+imc = peso / (altura * altura);
+
+printf ("%s, seu peso eh: %.2f, sua altura eh %.2f e seu imc eh %.2f.", nome, peso, altura, imc);
+return imc;
+}
